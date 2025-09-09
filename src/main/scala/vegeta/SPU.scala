acae4fb14f37ee021df2fbb25df9f07261b1065a
jennymarip <jennymarip830@gmail.com>
2025-09-09 10:02:51 +0800

    add unit test for SPU

--- src/main/scala/vegeta/SPU.scala
+++ src/main/scala/vegeta/SPU.scala
@@ -4,21 +4,19 @@ import chisel3._
 // SPU
 class SPU(num_mac : Int) extends Module {
     val io = IO(new Bundle {
-        val left_in = Input(Vec(num_mac, new data_block(num_mac)))
+        val left_in = Input(Vec(num_mac, Vec(4, SInt(32.W))))
         val up_in = Input(Vec(num_mac, SInt(32.W)))
         val weight_in = Input(Vec(num_mac, SInt(32.W)))
         val index_in = Input(Vec(num_mac, UInt(2.W)))
         val down_out = Output(Vec(num_mac, SInt(32.W)))
-        val weight_buffer_out = Output(Vec(num_mac, SInt(32.W)))
     })
     val weight_buffer = Reg(Vec(num_mac, SInt(32.W)))
-    io.weight_buffer_out := weight_buffer
     val weight_index = Reg(Vec(num_mac, UInt(2.W)))
     weight_buffer := io.weight_in
     weight_index := io.index_in
     val res = Reg(Vec(num_mac, SInt(32.W)))
     for(i <- 0 until num_mac)
-        res(i) := weight_buffer(i) * io.left_in(i).data(weight_index(i)) + io.up_in(i)
+        res(i) := weight_buffer(i) * io.left_in(i)(weight_index(i)) + io.up_in(i)
 
     io.down_out := res
 }
--- src/test/scala/SPUTest.scala
+++ src/test/scala/SPUTest.scala
@@ -8,14 +8,28 @@ class SPUTest extends AnyFlatSpec with ChiselScalatestTester{
     it should "pass" in {
         test(new SPU(2)) { u =>
           u.clock.setTimeout(0)
-          val weight_in = Seq(1, 2)
-          for(i <- 0 until 2)
+          val weight_in = Seq(11, 24)
+          val index_in = Seq(1, 2)
+          for(i <- 0 until 2){
               u.io.weight_in(i).poke(weight_in(i))
-          u.io.weight_buffer_out(0).expect(0)
-          u.io.weight_buffer_out(1).expect(0)
+              u.io.index_in(i).poke(index_in(i))
+          }
           u.clock.step(1)
-          u.io.weight_buffer_out(0).expect(1)
-          u.io.weight_buffer_out(1).expect(2)
+          val up_in = Seq(23, 34)
+          for(i <- 0 until 2){
+              u.io.up_in(i).poke(up_in(i))
+          }
+          val blk0 = Seq(10, 20, 30, 40)
+          val blk1 = Seq(12, 22, 32, 42)
+          val blks = Seq(blk0, blk1)
+          for (i <- 0 until 2){
+            for(j <- 0 until 4){
+                u.io.left_in(i)(j).poke(blks(i)(j))
+            }
+          }
+          u.clock.step(1)
+          u.io.down_out(0).expect(243)
+          u.io.down_out(1).expect(24*32+34)
         }
         println("SPU SUCCESS!!")
     }
